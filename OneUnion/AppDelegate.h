5c6df407cff60d604f6a4edbc7c8cd9c004bd891
dongnan <dongnan@didichuxing.com>
2016-03-25 17:08:33 +0800

    create

---
+++ OneUnion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneUnion
+//
+//  Created by 埋雷达人 on 16/3/25.
+//  Copyright © 2016年 didichuxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
