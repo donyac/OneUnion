1d3f45b0a3472ff6a14184aef357bd5dd656a32f
dongnan <dongnan@didichuxing.com>
2016-03-26 21:52:56 +0800

    铲除了Viewmanager，使用naviaition Controller控制页面跳转

---
+++ OneUnion/MainView.m
@@ -0,0 +1,21 @@
+//
+//  MainView.m
+//  OneUnion
+//
+//  Created by 埋雷达人 on 16/3/26.
+//  Copyright © 2016年 didichuxing. All rights reserved.
+//
+
+#import "MainView.h"
+
+@implementation MainView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ OneUnion/TopicsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopicsTableViewController.h
+//  OneUnion
+//
+//  Created by 埋雷达人 on 16/3/26.
+//  Copyright © 2016年 didichuxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicsTableViewController : UITableViewController
+
+@end
