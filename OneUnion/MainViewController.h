92d1131f3e944fbdd9089db0c931954498c1428b
dongnan <dongnan@didichuxing.com>
2016-03-26 21:12:37 +0800

    创建了主线程中的单Viewmanger

---
+++ OneUnion/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  OneUnion
+//
+//  Created by 埋雷达人 on 16/3/26.
+//  Copyright © 2016年 didichuxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ OneUnion/ViewManager.m
@@ -0,0 +1,13 @@
+//
+//  ViewManager.m
+//  OneUnion
+//
+//  Created by 埋雷达人 on 16/3/25.
+//  Copyright © 2016年 didichuxing. All rights reserved.
+//
+
+#import "ViewManager.h"
+
+@implementation ViewManager
+
+@end
