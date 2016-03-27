893b2d28a03da11a878d69db43724e40100d52e5
dongnan <dongnan@didichuxing.com>
2016-03-27 13:08:23 +0800

    no message

--- OneUnion/TopicTableViewCell.h
+++ OneUnion/TopicTableViewCell.h
@@ -1,13 +1,13 @@
 //
-//  TopicsView.h
+//  TopicTableViewCell.h
 //  OneUnion
 //
-//  Created by 埋雷达人 on 16/3/26.
+//  Created by 埋雷达人 on 16/3/27.
 //  Copyright © 2016年 didichuxing. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface TopicsView : UIView
+@interface TopicTableViewCell : UITableViewCell
 
 @end
---
+++ OneUnion/TopicTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  TopicTableViewCell.m
+//  OneUnion
+//
+//  Created by 埋雷达人 on 16/3/27.
+//  Copyright © 2016年 didichuxing. All rights reserved.
+//
+
+#import "TopicTableViewCell.h"
+
+@implementation TopicTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
